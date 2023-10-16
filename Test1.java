6aef1abd7746aed00599821628660b02bb2490e0
shootingstartf89 <nanavati.pankti@gmail.com>
2023-10-17 00:05:38 +0100

    Push Testing

---
+++ Test1.java
@@ -0,0 +1,14 @@
+// Write a program to print BioData
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("BioData");
+        System.out.println("My name is Pankti Nanavati");
+        System.out.println("I have completed Masters in Electronics and Communication");
+        System.out.println("I have been working as a Software tester");
+        System.out.println("I have experience of Manual and Automation Testing");
+        System.out.println("I have worked with Gherkin,Postman");
+
+
+    }
+}
+
